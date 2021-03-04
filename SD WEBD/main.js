56fa0c63fb0d0f373f66009381644745f2ac5c65
Subhrajit-D <56694690+Subhrajit-D@users.noreply.github.com>
2021-03-05 03:50:37 +0530

    Add files via upload

---
+++ SD WEBD/main.js
@@ -0,0 +1,11 @@
+$(".slider-one")
+.not(".slick-initialized")
+.slick({
+
+	prevArrow:".site-slider.prev",
+	nextArrow: ".site-slider.next",
+	slidesToShow: 5,
+	slidesToScroll: 1,
+	autoplaySpeed: 1900,
+        background-color:black
+});
